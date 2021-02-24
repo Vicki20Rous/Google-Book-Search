a897b3ec3ec1962a124eec87879cd4b8dab4f5aa
Victoria Rouser <vic86rous@gmail.com>
2021-02-24 10:04:02 -0600

    fresh start

---
+++ src/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+    <header>
+        <i class="far fa-apple-crate"></i>
+        <h1>Google Book Search</h1>
+    </header>
+    )
+}
+
+export default Header;
